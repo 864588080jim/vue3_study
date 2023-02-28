d600e41b9cdc2e5d5cff1fa83bf0be0ce914fda5
BingYunpeng <864588080@qq.com>
2023-02-28 10:29:16 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,22 @@
 module.exports = {
   base: '/typescript/', /* 基础虚拟路径 */
   dest: 'docs/dist', /* 打包文件基础路径, 在命令所在目录下 */
-  title: 'Vue3+TS 快速上手123456', // 标题
+  title: 'Vue3+TS 快速上手', // 标题
   description: '尚硅谷前端研究院', // 标题下的描述
   themeConfig: { // 主题配置
-    logo: '/images/logo.png',
+    // logo: '/images/logo.png',
     nav: [
-      { text: '官网', link: 'http://www.atguigu.com' },
-      { text: '谷粒学院', link: 'http://www.gulixueyuan.com/' },
+      { text: 'js', link: 'http://spaceofbing.cn/js' },
+      { text: 'es6', link: 'http://spaceofbing.cn/es6' },
       {
-        text: '学习路线',
+        text: 'vue3',
         items: [
-          { text: '前端', link: 'http://www.atguigu.com/web/' },
-          { text: 'Java', link: 'http://www.atguigu.com/kecheng.shtml' },
-          { text: '大数据', link: 'http://www.atguigu.com/bigdata/' }
+          { text: 'vue3', link: 'http://spaceofbing.cn/vue3' },
+          { text: 'router', link: 'http://spaceofbing.cn/router' },
+          { text: 'pinia', link: 'http://spaceofbing.cn/pinia' }
         ]
       },
-      { text: '全套视频资料', link: 'http://www.gulixueyuan.com/' },
+      { text: 'node', link: 'http://spaceofbing.cn/node' },
     ],
     sidebar: [ // 左侧导航
       '00_课程介绍',
@@ -85,6 +85,6 @@ module.exports = {
   },
 
   head: [
-    ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `./images/favicon.ico` }]
+    // ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `./images/favicon.ico` }]
   ]
 }
